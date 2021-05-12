28387277e670656fed87d8382bb2a3253c2b9262
kevin clarke <kevinpatrickclarke@gmail.com>
2021-05-12 10:35:10 -0400

    adding txt file for high scores

--- scoreboard.py
+++ scoreboard.py
@@ -5,14 +5,21 @@ FONT = ('Courier', 18, 'normal')
 class ScoreBoard(Turtle):
 	def __init__(self):
 		super().__init__()
-
 		self.score = 0
-		self.high_score = 0
+		self.high_score = self.read_high_score()
 		self.color('yellow')
 		self.penup()
 		self.goto(0, 270)
 		self.hideturtle()
 		self.update_scoreboard()
+
+	def read_high_score(self):
+		with open('scores.txt') as f:
+			return int(f.read()) or 0
+
+	def update_high_score(self, score):
+		with open('scores.txt', 'w') as f:
+			f.write(str(score))
 
 	def increment_score(self):
 		self.score += 1
@@ -25,6 +32,7 @@ class ScoreBoard(Turtle):
 	def reset(self):
 		if(self.score > self.high_score):
 			self.high_score = self.score
+			self.update_high_score(self.high_score)
 		self.score = 0
 		self.update_scoreboard()
 
